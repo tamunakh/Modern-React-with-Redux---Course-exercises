d58a0280201902a9998adc5367d6012fe0c45705
Tamar Khojamirova <tkhojamirova@vaba.co>
2021-01-21 17:58:42 +0400

    html vs. jsx

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,3 +1,30 @@
-() => {
-    console.log('hi');
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// function component
+const App = () => {
+    return (
+        <div>
+            {/* In jsx we use html for instead of for */}
+            <label className="label" htmlFor="name">
+                Enter name:
+            </label>
+            <input id="name" type="text"/>
+            {/* difference betweeen adding styles with html and jsx
+            html :
+            <button style="color: white; background-color: blue;">
+                submit
+            </button>
+            jsx:
+            */}
+            <button style={{color: 'white', backgroundColor: 'blue'}}>
+                submit
+            </button>
+        </div>
+    );
+};
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
